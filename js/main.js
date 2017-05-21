1e9bcd0b87a66f73a013416ac1f5cb09335113d9
Thetawaves <jessica.granziera@gmail.com>
2017-05-21 21:18:19 +0200

    To be fixed: -Responsiveness -Loading Screen

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    
     $('.container').click(function () {
         $('.container').removeClass('containerClicked');
         $('.container .bot').removeClass('botClicked');
@@ -8,6 +9,7 @@ $(document).ready(function () {
         $('.container .top img').css({
             display: 'block'
         });
+        $('.role-container #close-menu').removeClass('closeMenu');
         $(this).addClass('containerClicked');
         $(this).find('.bot').addClass('botClicked');
         $(this).find('.card').addClass('cardClicked');
@@ -15,11 +17,29 @@ $(document).ready(function () {
         $(this).find('.section').addClass('removeBorder');
         $(this).find('.top img').css({
             display: 'none'
-        })
+        });
+        $(this).prev().addClass('closeMenu')
     });
     
+    $('.role-container #close-menu').click(function () {
+        $('.container').removeClass('containerClicked');
+        $('.container .bot').removeClass('botClicked');
+        $('.container .bot .card').removeClass('cardClicked');
+        $('.container .bot .card h2').removeClass('nameClicked');
+        $('.container .section').removeClass('removeBorder');
+        $('.container .top img').css({
+            display: 'block'
+        });
+        $('.role-container #close-menu').removeClass('closeMenu');
+    });
     
-  $('.slicker').slick({
+    $('.slicker').slick({
       
-  });
+    });
+    
+    $(".scroll").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+    
 })
