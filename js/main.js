2fc08964d430ba0485f34ee4ee7babc5b1bfde15
Thetawaves <jessica.granziera@gmail.com>
2017-05-25 07:23:07 +0200

    Fixed: -Loading Screen To be Fixed: -Responsiveness

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    $('#loading').delay(1500).fadeOut(500);
+    
+    $("a.scroll").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
     
     $('.container').click(function () {
         $('.container').removeClass('containerClicked');
@@ -16,7 +22,7 @@ $(document).ready(function () {
         $(this).find('h2').addClass('nameClicked');
         $(this).find('.section').addClass('removeBorder');
         $(this).find('.top img').css({
-            display: 'none'
+            visibility: 'hidden'
         });
         $(this).prev().addClass('closeMenu')
     });
@@ -28,18 +34,32 @@ $(document).ready(function () {
         $('.container .bot .card h2').removeClass('nameClicked');
         $('.container .section').removeClass('removeBorder');
         $('.container .top img').css({
-            display: 'block'
+            visibility: 'visible'
         });
         $('.role-container #close-menu').removeClass('closeMenu');
     });
     
-    $('.slicker').slick({
-      
+    $(document).scroll(function () {
+        var lastScrollTop = 0;
+        var st = $(this).scrollTop();
+        var bottom = $(document).height();
+    if (st > lastScrollTop) {
+        $('#arrow-back').fadeOut(500);
+        $('#arrow-top').fadeIn(500);
+        $('.arrow').addClass('rotate');
+    } else if (st == bottom) {
+        $('arrow').fadeOut(500);
+    } else {
+        $('#arrow-back').fadeIn(500);
+        $('#arrow-top').fadeOut(500);
+        $('.arrow').removeClass('rotate');
+    };
+        console.log(st);
+        console.log(bottom);
     });
     
-    $(".scroll").click(function(event){     
-        event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    $('.slicker').slick({
+      
     });
     
 })
